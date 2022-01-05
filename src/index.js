2758151c2f5a3ad9fd3d40a1a64f7444ff2605a9
zhangy <1098481875@qq.com>
2022-01-05 22:08:50 +0800

    fix useContext input

--- src/index.js
+++ src/index.js
@@ -1,57 +1,51 @@
-import React, { useReducer } from "react";
+import React, { useContext, useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-// const Navbar = () => {
-//   const { userName } = useContext(AppContext);
+const AppContext = React.createContext({});
 
-//   return (
-//     <div className="navbar">
-//       <p>AwesomeSite</p>
-//       <p>{userName}</p>
-//     </div>
-//   );
-// };
+const Navbar = () => {
+  const { userName } = useContext(AppContext);
+
+  return (
+    <div className="navbar">
+      <p>AwesomeSite</p>
+      <p>{userName}</p>
+    </div>
+  );
+};
 
 const Messages = () => {
-  // const myreducer = (state,action)=> {
-  //   switch(action.type){
-  //     case 'update':
-  //       console.log(state)
-  //       return state;
-  //     default:
-  //       return state;
-  //   }
-  // }
-  // const [userName, dispath] = useReducer(myreducer, 'x');
-  const myreducer = (state,action)=> {
-    switch(action.type){
-        case 'add':
-             return {...state,count: state.count+1};
-         case 'sub':
-             return state - 1;
-         default:
-             return state;
-    }
+  const { userName, setUserName } = useContext(AppContext);
+  function formChange(e) {
+    console.log(e.target.value);
+    setUserName(e.target.value);
+    // username = e.target.value
   }
-  const [obj, dispath] = useReducer(myreducer, {count: 0});
   return (
     <div className="messages">
-      {/* <h1>Messages</h1>
+      <h1>Messages</h1>
       <p>1 message for {userName}</p>
       <p className="message">useContext is awesome!</p>
-      <input value={userName} onChange={(e) =>dispath({type: 'update'})} /> */}
-      <span>{obj.count}</span>
-      <button onClick={()=>dispath({type: 'add'})}>Increment</button>
+      <input value={userName} onChange={(e) => formChange(e)} />
     </div>
   );
 };
 
 function App() {
+  const [userName, setUserName] = useState("defaultValue");
   return (
-    <div className="App">
-      <Messages />
-    </div>
+    <AppContext.Provider
+      value={{
+        userName,
+        setUserName
+      }}
+    >
+      <div className="App">
+        <Navbar />
+        <Messages />
+      </div>
+    </AppContext.Provider>
   );
 }
 
