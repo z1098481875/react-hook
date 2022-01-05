1c6b92b8d17545ca9fd7abbb26308a0441fa0bb9
zhangy <1098481875@qq.com>
2022-01-05 23:44:17 +0800

    fix  useReducer  login

--- src/index.js
+++ src/index.js
@@ -1,56 +1,66 @@
-import React, { useReducer } from "react";
+import React, { useReducer, Component } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-// const Navbar = () => {
-//   const { userName } = useContext(AppContext);
-
-//   return (
-//     <div className="navbar">
-//       <p>AwesomeSite</p>
-//       <p>{userName}</p>
-//     </div>
-//   );
-// };
-
-const Messages = () => {
-  // const myreducer = (state,action)=> {
-  //   switch(action.type){
-  //     case 'update':
-  //       console.log(state)
-  //       return state;
-  //     default:
-  //       return state;
-  //   }
-  // }
-  // const [userName, dispath] = useReducer(myreducer, 'x');
-  const myreducer = (state,action)=> {
-    switch(action.type){
-        case 'add':
-             return {...state,count: state.count+1};
-         case 'sub':
-             return state - 1;
-         default:
-             return state;
-    }
+const myreducer = (state,action)=> {
+  switch(action.type){
+    case 'userName':
+      return {...state,userName: action.userName}
+    case 'password':
+      return {...state,password: action.password}
+    default:
+      return state;
   }
-  const [obj, dispath] = useReducer(myreducer, {count: 0});
+}
+
+const UserName = (props) => {
+  let {state, dispatch} = props
+  let {userName, password} = state
   return (
     <div className="messages">
-      {/* <h1>Messages</h1>
+      <h1>Messages</h1>
       <p>1 message for {userName}</p>
-      <p className="message">useContext is awesome!</p>
-      <input value={userName} onChange={(e) =>dispath({type: 'update'})} /> */}
-      <span>{obj.count}</span>
-      <button onClick={()=>dispath({type: 'add'})}>Increment</button>
+      <p className="message">{password}</p>
+      <input value={userName} onChange={(e) =>dispatch({type: 'userName', userName: e.target.value})} />
     </div>
   );
 };
 
+const PassWord = (props) => {
+  let {state, dispatch} = props
+  let {password, userName} = state
+  return (
+    <div className="navbar">
+      <p>{userName}</p>
+      <input value={password} onChange={(e) =>dispatch({type: 'password', password: e.target.value})} />
+    </div>
+  );
+};
+
+class Button extends Component {
+  handleClick =() =>{
+    let {state} = this.props
+    let {password, userName} = state
+    console.log(userName, password)
+    fetch('www/getData', {method:'POST'
+      ,headers:{ 'Content-Type': 'application/json' }
+      ,body: JSON.stringify({ userName, password})
+    }).then( function (resp) {
+      console.log(resp);
+      return resp.blob() })
+  }
+  render() {
+    return <button onClick={this.handleClick}>提交</button>;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(myreducer, {userName:'', password: ''});
   return (
     <div className="App">
-      <Messages />
+      <UserName dispatch={dispatch} state={state}/>
+      <PassWord dispatch={dispatch} state={state}/>
+      <Button state={state}/>
     </div>
   );
 }
